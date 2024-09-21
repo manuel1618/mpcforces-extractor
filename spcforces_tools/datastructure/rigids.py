027d411ddef27d768cc91f546110f86f0d84061e
Manuel <manuel.ramsaier89@gmail.com>
2024-09-21 23:25:42 +0200

    basic implementation on playground

--- spcforces_tools/datastructure/rigids.py
+++ spcforces_tools/datastructure/rigids.py
@@ -16,6 +16,55 @@ class MPC:
         self.dofs: int = dofs
         self.property2nodes: Dict = {}
 
+    def sum_forces_by_connected_parts(self, node_id2force: Dict):
+        """
+        This method is used to sum the forces by connected - parts NEW
+        """
+        forces = {}
+        part_id2node_ids = {}
+
+        # get the connected nodes for each part (attached elements)
+        node_stack = self.nodes.copy()
+        part_id = 1
+        while len(node_stack) > 0:
+            node = node_stack.pop()
+            element = node.connected_elements[0]
+            connected_nodes = element.get_all_connected_nodes()
+            part_nodes = set(connected_nodes).intersection(self.nodes)
+            for node_temp in part_nodes:
+                if node_temp in node_stack:
+                    node_stack.remove(node_temp)
+            part_id2node_ids[part_id] = [node.id for node in part_nodes]
+            part_id += 1
+
+        # add the forces for each part
+        for part_id, node_ids in part_id2node_ids.items():
+            forces[part_id] = [0, 0, 0, 0, 0, 0]
+
+            for node_id in node_ids:
+                if node_id not in node_id2force:
+                    print(
+                        f"Node {node_id} not found in the MPC forces file - bug or zero - you decide!"
+                    )
+                    continue
+
+                force = node_id2force[node_id]
+                force_x = force[0]
+                force_y = force[1]
+                force_z = force[2]
+                moment_x = force[3]
+                moment_y = force[4]
+                moment_z = force[5]
+
+                forces[part_id][0] += force_x
+                forces[part_id][1] += force_y
+                forces[part_id][2] += force_z
+                forces[part_id][3] += moment_x
+                forces[part_id][4] += moment_y
+                forces[part_id][5] += moment_z
+
+        return forces
+
     def sum_forces_by_property(self, node2force: Dict):
         """
         This method is used to sum the forces by property
