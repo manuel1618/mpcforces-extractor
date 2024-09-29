e11fa5751a897857cb5cb7ea13fa87b3e1270f7f
manuel1618 <manuel.ramsaier89@gmail.com>
2024-09-29 16:04:37 +0200

    loads tests

--- mpcforces_extractor/datastructure/test_rigids.py
+++ mpcforces_extractor/datastructure/test_rigids.py
@@ -1,5 +1,6 @@
 import unittest
 from mpcforces_extractor.datastructure.rigids import MPC, MPC_CONFIG
+from mpcforces_extractor.datastructure.entities import Node
 
 
 class TestRigids(unittest.TestCase):
@@ -19,3 +20,41 @@ class TestRigids(unittest.TestCase):
         self.assertEqual(mpc.element_id, 1)
         self.assertEqual(mpc.nodes, [1, 2])
         self.assertEqual(mpc.dofs, "123")
+
+    def test_sum_forces_by_connected_parts(self):
+
+        node_id2force = {
+            1: [1, 1, 1, 0, 0, 0],
+            2: [2, 2, 2, 0, 0, 0],
+        }
+        part_id2connected_node_ids = {
+            1: [1, 2],
+            2: [2, 3],
+        }
+
+        node1 = Node(
+            node_id=1,
+            coords=[0, 0, 0],
+        )
+        node2 = Node(
+            node_id=2,
+            coords=[0, 0, 0],
+        )
+        node3 = Node(
+            node_id=3,
+            coords=[0, 0, 0],
+        )
+
+        mpc = MPC(
+            element_id=10,
+            mpc_config=MPC_CONFIG.RBE2,
+            master_node=0,
+            nodes=[node1, node2, node3],
+            dofs="123",
+        )
+
+        forces = mpc.sum_forces_by_connected_parts(
+            node_id2force, part_id2connected_node_ids
+        )
+
+        self.assertTrue(forces[1] == [3, 3, 3, 0, 0, 0])
