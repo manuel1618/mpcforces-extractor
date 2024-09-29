86d425cfffb1544598f1cf27567ec673b04e7af5
manuel1618 <manuel.ramsaier89@gmail.com>
2024-09-29 22:37:04 +0200

    basic implementation

--- mpcforces_extractor/main.py
+++ mpcforces_extractor/main.py
@@ -1,4 +1,7 @@
+import os
+import time
 from mpcforces_extractor.force_extractor import MPCForceExtractor
+from mpcforces_extractor.visualize.tcl_visualize import VisualizerConnectedParts
 
 
 def main():
@@ -20,7 +23,17 @@ def main():
 
     rigidelement2forces = mpc_force_extractor.get_mpc_forces(blocksize)
     mpc_force_extractor.write_suammry(rigidelement2forces)
-    mpc_force_extractor.write_tcl_vis_lines()
+
+    # Visualize the connected parts
+    start_time = time.time()
+    output_vis = os.path.join(output_folder, "tcl_visualization")
+    visualizer = VisualizerConnectedParts(
+        mpc_force_extractor.part_id2connected_node_ids, output_vis
+    )
+    visualizer.output_tcl_lines_for_part_vis()
+
+    print("TCL visualization lines written to", output_vis)
+    print("..took ", round(time.time() - start_time, 2), "seconds")
 
 
 if __name__ == "__main__":
