4a84ad6ba6c9f1b7d4fc9130e3ab3af554efe0cf
manuel1618 <manuel.ramsaier89@gmail.com>
2024-10-11 23:38:25 +0200

    add a simple frontend

--- mpcforces_extractor/playground/api.py
+++ mpcforces_extractor/playground/api.py
@@ -1,8 +1,37 @@
-from fastapi import FastAPI, HTTPException, status
+from typing import List
+from fastapi import FastAPI, HTTPException, status, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from mpcforces_extractor.playground.database import FakeMPCDatabase, MPCDBModel
 
 app = FastAPI()
 db = FakeMPCDatabase()
+
+# Mount the static files directory
+app.mount(
+    "/static",
+    StaticFiles(directory="mpcforces_extractor/playground/frontend/static"),
+    name="static",
+)
+
+
+# Setup Jinja2 templates
+templates = Jinja2Templates(
+    directory="mpcforces_extractor/playground/frontend/templates"
+)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    """Render the index.html template"""
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/api/v1/mpcs", response_model=List[MPCDBModel])
+async def get_mpcs() -> List[MPCDBModel]:
+    """Get all MPCs"""
+    return await db.get_mpcs()
 
 
 @app.get("/api/v1/mpcs/{id}", response_model=MPCDBModel)
