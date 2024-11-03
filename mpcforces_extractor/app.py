a84ba3f0327710a5ca9ec058c70b62912ab6da55
manuel1618 <manuel.ramsaier89@gmail.com>
2024-11-03 23:42:28 +0100

    made the app the default way to use it

--- mpcforces_extractor/app.py
+++ mpcforces_extractor/app.py
@@ -1,5 +1,13 @@
 import uvicorn
 from mpcforces_extractor.api.main import app
 
-if __name__ == "__main__":
+
+def main():
+    """
+    This is the main function that is used to run MPCForceExtractor
+    """
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+if __name__ == "__main__":
+    main()
