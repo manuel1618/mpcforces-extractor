5927d3e4597488c8bd06aa876809459ec87107e5
Manuel <manuel.ramsaier89@gmail.com>
2024-11-04 23:18:15 +0100

    working solution with the os.getcwd() solution

--- mpcforces_extractor/api/config.py
+++ mpcforces_extractor/api/config.py
@@ -1,10 +1,23 @@
+import os
 from pathlib import Path
 
 ITEMS_PER_PAGE = 100
+
+# Wwebserer Folders
 PACKAGE_ROOT_DIR = Path(__file__).resolve().parent.parent
-UPLOAD_FOLDER = f"{PACKAGE_ROOT_DIR}/data/uploads"
-OUTPUT_FOLDER = f"{PACKAGE_ROOT_DIR}/data/output"
 STATIC_DIR = f"{PACKAGE_ROOT_DIR}/frontend/static"  # Path to the static directory
 TEMPLATES_DIR = (
     f"{PACKAGE_ROOT_DIR}/frontend/templates"  # Path to the templates directory
 )
+
+# Data Folders
+CWD = os.getcwd()
+DATA_DIR = f"{CWD}/data"
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+UPLOAD_FOLDER = f"{DATA_DIR}/uploads"
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+OUTPUT_FOLDER = f"{DATA_DIR}/output"
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
