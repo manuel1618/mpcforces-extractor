9abc228855ee8c9bb0de01ccdd35cf5f5ac9e3e8
manuel1618 <manuel.ramsaier89@gmail.com>
2024-12-21 15:51:33 +0100

    tidy up - util class, threadpoolexecuter finally giving results

---
+++ mpcforces_extractor/reader/reader_utilities.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class modelReaderUtilities:
+    """
+    This class contains methods for the model reader which
+    are just utility methods and could be used by other readers as well
+    """
+
+    @staticmethod
+    def node_coord_parser(coord_str: str) -> float:
+        """
+        This method is used to parse the node coordinates
+        """
+        # Problem is the expenential notation without the E in it
+        before_dot = coord_str.split(".")[0]
+        after_dot = coord_str.split(".")[1]
+        if "-" in after_dot:
+            return float(before_dot + "." + after_dot.replace("-", "e-"))
+        if "+" in after_dot:
+            return float(before_dot + "." + after_dot.replace("+", "e+"))
+
+        return float(coord_str)
+
+    @staticmethod
+    def split_line(line: str, blocksize: int) -> List:
+        """
+        This method is used to split a line into blocks of blocksize, and
+        remove the newline character and strip the content of the block
+        """
+        line_content = [line[j : j + blocksize] for j in range(0, len(line), blocksize)]
+
+        if "\n" in line_content:
+            line_content.remove("\n")
+
+        line_content = [line.strip() for line in line_content]
+        return line_content
