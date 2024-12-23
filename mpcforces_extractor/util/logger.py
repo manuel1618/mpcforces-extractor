7b824e31ec7f5ad3ecfed10afe5bcb0cdc2a15bc
Manuel <manuel.ramsaier89@gmail.com>
2024-12-23 12:39:16 +0100

    review requested change

--- mpcforces_extractor/util/logger.py
+++ mpcforces_extractor/util/logger.py
@@ -13,7 +13,6 @@ class Logger:
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-
             with cls._lock:
                 if not cls._instance:
                     cls._instance = super(Logger, cls).__new__(cls, *args, **kwargs)
